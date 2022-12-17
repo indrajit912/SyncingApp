449f6120de17b61e0ee07285111317acac711a91
Indrajit <indrajitghosh912@gmail.com>
2022-12-17 11:33:07 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Project to synchronize data between my laptop and external HDD
+#
+# Author: Indrajit Ghosh
+#
+# Created on: Dec 17, 2022
+#
+
+
+def main():
+    print('hello world!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ sync/functions.py
@@ -0,0 +1,36 @@
+# Functions required for the project Sync
+#
+# Author: Indrajit Ghosh
+#
+# Created on: Dec 17, 2022
+#
+
+import getpass
+from pathlib import Path
+
+
+def current_username():
+    """
+    Returns the current username
+
+    Returns:
+    --------
+        `str`
+    """
+    return getpass.getuser()
+
+
+def media_path(drive_name:str=None):
+    """
+    Returns the `Path(/media/{username}/{drive_name})`
+    """
+    drive_name = '' if drive_name is None else drive_name
+    return Path(f"/media/{current_username()}/{drive_name}")
+
+
+def main():
+    print(media_path())
+
+
+if __name__ == '__main__':
+    main()
