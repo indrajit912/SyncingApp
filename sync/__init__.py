b4ecb2cf902936bb9afc1a579e8aff9cb30bf553
Indrajit <indrajitghosh912@gmail.com>
2022-12-18 16:14:59 +0530

    syncer.py renamed to utils.py

--- sync/__init__.py
+++ sync/__init__.py
@@ -1,2 +1,2 @@
 from .functions import *
-from .syncer import *
+from .utils import *
--- testings.py
+++ testings.py
@@ -12,23 +12,21 @@
 # Get file sizes by os.stat()
 
 from sync import *
-
-BACKUP_DIR = Path.home() / "Downloads" / "backup_test_dir"
-SOURCE_DIR = Path.home() / "Downloads" / "source_test_dir"
-F1 = SOURCE_DIR / "sound_waves.mp4"
-F2 = SOURCE_DIR / "Assignment_2_FuncAnalysis_Mmat1_2021_22.pdf"
-F3 = SOURCE_DIR / "spam.txt"
-PIC = SOURCE_DIR / "holi.jpg"
+DOWNLOADS = Path.home() / "Downloads"
 
 
 def main():
 
-    syncer = Syncer(
-        nodes=[SOURCE_DIR, BACKUP_DIR, Path.home() / 'Downloads/indra/'],
-        name="IndraSync"
-    )
+    nodes = [
+        DOWNLOADS / "hey",
+        DOWNLOADS / "nice",
+        DOWNLOADS / "japan"
+    ]
+
+    syncer = Syncer(nodes=nodes, name="TestSync")
 
     syncer.sync_nodes()
+
 
 
 if __name__ == '__main__':
